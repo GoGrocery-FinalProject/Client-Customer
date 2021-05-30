221ca9ef34c48be3d7987185072701a6f33fa7bf
RezaHabrizal <jreza.habrizal@gmail.com>
2021-05-30 21:53:33 +0700

    midtrans
    add method di actionbox nembak server untuk get token dan direct link, add pages MidtransWebView, navigate ke MidtransWebView bawa params link

--- Kanjur-Mobile-App/App.js
+++ Kanjur-Mobile-App/App.js
@@ -1,40 +1,70 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React from 'react'
+import { StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import Home from './src/Screen/Home';
-import Login from './src/Screen/Login';
+import Home from './src/Screen/Home'
+import Login from './src/Screen/Login'
 import { Provider as ReduxProvider } from 'react-redux'
-import { store } from './store';
-import Splash from './src/Screen/Splash';
-import Register from './src/Screen/Register';
-import Transaction from './src/Screen/Transaction';
-import CheckIn from './src/Screen/CheckIn';
+import { store } from './store'
+import Splash from './src/Screen/Splash'
+import Register from './src/Screen/Register'
+import Transaction from './src/Screen/Transaction'
+import CheckIn from './src/Screen/CheckIn'
+import MidtransWebView from './src/Screen/MidtransWebView'
 
 export default function App() {
-  const Stack = createStackNavigator()
+	const Stack = createStackNavigator()
 
-  return (
-    <ReduxProvider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
-          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-          <Stack.Screen name="Login" component={Login} options={{ headerStyle: {backgroundColor: '#0095DA'}}}/>
-          <Stack.Screen name="Register" component={Register} options={{ headerStyle: {backgroundColor: '#FB5533'}}} />
-          <Stack.Screen name="CheckIn" component={CheckIn} options={{ headerShown: false }} />
-          <Stack.Screen name="Transaction" component={Transaction} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ReduxProvider>
-  );
+	return (
+		<ReduxProvider store={store}>
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen
+						name="Splash"
+						component={Splash}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="Home"
+						component={Home}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="Login"
+						component={Login}
+						options={{ headerStyle: { backgroundColor: '#0095DA' } }}
+					/>
+					<Stack.Screen
+						name="Register"
+						component={Register}
+						options={{ headerStyle: { backgroundColor: '#FB5533' } }}
+					/>
+					<Stack.Screen
+						name="CheckIn"
+						component={CheckIn}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="Transaction"
+						component={Transaction}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="MidtransWebView"
+						component={MidtransWebView}
+						options={{ headerShown: false }}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</ReduxProvider>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
--- Kanjur-Mobile-App/src/Component/ActionBox.jsx
+++ Kanjur-Mobile-App/src/Component/ActionBox.jsx
@@ -1,78 +1,111 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { IconButton } from 'react-native-paper';
-import { heightPercentageToDP, widthPercentageToDP } from 'react-native-responsive-screen';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { IconButton } from 'react-native-paper'
+import {
+	heightPercentageToDP,
+	widthPercentageToDP,
+} from 'react-native-responsive-screen'
 import ButtonView from '../Component/Button'
+import axios from 'axios'
 
-export default function ActionBox({navigation}) {
-    const [shadowOffsetWidth, setShadowOffsetWidth] = useState(8);
-    const [shadowOffsetHeight, setShadowOffsetHeight] = useState(6);
-    const [shadowRadius, setShadowRadius] = useState(25);
-    const [shadowOpacity, setShadowOpacity] = useState(1);
-    
-    function ScanMethod() {
-        console.log('coba')
-    }
+export default function ActionBox({ navigation }) {
+	const [shadowOffsetWidth, setShadowOffsetWidth] = useState(8)
+	const [shadowOffsetHeight, setShadowOffsetHeight] = useState(6)
+	const [shadowRadius, setShadowRadius] = useState(25)
+	const [shadowOpacity, setShadowOpacity] = useState(1)
+	const [token, setToken] = useState('')
+	const [link, setLink] = useState('')
 
-    function payment() {
-        console.log('bayar');
-    }
+	function ScanMethod() {
+		console.log('coba')
+	}
 
-    function history() {
-        console.log('riwayat');
-    }
+	function payment() {
+		axios({
+			url: 'http://192.168.100.14:3000',
+			method: 'POST',
+			data: {
+				gross_amount: 100000, // total gross dari cart
+				item_details: [
+					// item detail bisa kirim cart nya require arr of obj
+					{
+						id: 'ITEM1',
+						price: 100000,
+						quantity: 1,
+						name: 'Midtrans Bear',
+						brand: 'Midtrans',
+						category: 'Toys',
+						merchant_name: 'Midtrans',
+					},
+				],
+			},
+		})
+			.then(({ data }) => {
+				setLink(data.link)
+				setToken(data.transactionToken)
+				navigation.navigate('MidtransWebView', {
+					link,
+				})
+			})
+			.catch(console.log)
+		console.log('bayar')
+	}
 
-    function checkout() {
-        console.log('checkout');
-    }
+	function history() {
+		console.log('riwayat')
+	}
 
-    return (
-        <View style={[
-        styles.actionBox,
-            {
-            shadowOffset: {
-                width: shadowOffsetWidth,
-                height: -shadowOffsetHeight
-            },
-            shadowOpacity,
-            shadowRadius
-            }
-        ]}>
-            <ButtonView method={ScanMethod} icon="barcode-scan" title="Scan"/>
-            <ButtonView method={payment} icon="cash" title="Bayar"/>
-            <ButtonView method={history} icon="history" title="Riwayat"/>
-            <ButtonView method={checkout} icon="logout" title="Keluar"/>
-        </View>
-    );
+	function checkout() {
+		console.log('checkout')
+	}
+
+	return (
+		<View
+			style={[
+				styles.actionBox,
+				{
+					shadowOffset: {
+						width: shadowOffsetWidth,
+						height: -shadowOffsetHeight,
+					},
+					shadowOpacity,
+					shadowRadius,
+				},
+			]}
+		>
+			<ButtonView method={ScanMethod} icon="barcode-scan" title="Scan" />
+			<ButtonView method={payment} icon="cash" title="Bayar" />
+			<ButtonView method={history} icon="history" title="Riwayat" />
+			<ButtonView method={checkout} icon="logout" title="Keluar" />
+		</View>
+	)
 }
 
-const styles = StyleSheet.create({  
-  actionBox: {
-    backgroundColor: '#fff',
-    shadowColor: 'black',
-    position: 'absolute',
-    height: heightPercentageToDP('16%'),
-    width: widthPercentageToDP('90%'),
-    left: widthPercentageToDP('5%'),
-    top: heightPercentageToDP('21%'),
-    borderRadius: 5,
-    flexDirection: 'row',
-    textAlign: 'center',
-    justifyContent: 'space-evenly'
-  },
-  button: {
-
-  },
-  icon : {
-    margin: 0,
-    padding: 0
-  },
-  buttonText: {
-      color: '#0095DA',
-      fontFamily: 'Roboto',
-      fontSize: widthPercentageToDP('4%'),
-      textAlign: 'center',
-      padding: 0,
-      margin: 0
-  }
-});
+const styles = StyleSheet.create({
+	actionBox: {
+		backgroundColor: '#fff',
+		shadowColor: 'black',
+		position: 'absolute',
+		height: heightPercentageToDP('16%'),
+		width: widthPercentageToDP('90%'),
+		left: widthPercentageToDP('5%'),
+		top: heightPercentageToDP('21%'),
+		borderRadius: 5,
+		flexDirection: 'row',
+		textAlign: 'center',
+		justifyContent: 'space-evenly',
+	},
+	button: {},
+	icon: {
+		margin: 0,
+		padding: 0,
+	},
+	buttonText: {
+		color: '#0095DA',
+		fontFamily: 'Roboto',
+		fontSize: widthPercentageToDP('4%'),
+		textAlign: 'center',
+		padding: 0,
+		margin: 0,
+	},
+})
